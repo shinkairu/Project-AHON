ded856547d0b681278fac94fa797cca7841834e6
shinkairu <keithdelcarmen1@gmail.com>
2026-01-17 15:46:13 +0800

    Add files via upload

---
+++ client/src/App.tsx
@@ -0,0 +1,33 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/not-found";
+import Dashboard from "@/pages/Dashboard";
+import Analytics from "@/pages/Analytics";
+import About from "@/pages/About";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Dashboard} />
+      <Route path="/analytics" component={Analytics} />
+      <Route path="/about" component={About} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Router />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
